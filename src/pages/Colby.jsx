558bfb3509f3bd38da301858728431b18d4286c5
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-01-22 00:40:45 -0500

    Add music background image to folder
    The music background will be placed in every chapter page for uniformity.

--- src/pages/Colby.jsx
+++ src/pages/Colby.jsx
@@ -15,6 +15,7 @@ const Colby = () => {
             <div style={{backgroundImage: `url(${chapterData[0].background})`}} className={`h-[25rem] bg-no-repeat bg-center w-full bg-cover ${styles.flexCenter}`}>
             <p className={`font-medium xs:text-[3rem] text-[2.5rem] text-white xs:leading-[76.8px] leading-[66.8px] w-full font-raleway text-center [text-shadow:2px_2px_2px_black] text-outline`}>{chapterData[0].header}</p>
             </div>
+            <h3>Officers</h3>
         </div>
     )
 }
