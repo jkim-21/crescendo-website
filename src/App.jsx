9982527714f0720924b8bef8eb85d3fa0b764873
jon-raymond1 <Jon.raymond2002@gmail.com>
2024-02-02 10:48:58 -0600

    Made changes to theme
    Stil Experimenting a lot!

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import styles from './style'
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    
+    <div className='bg-third w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+          
         </div>
       </div>
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-harvardBackground ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
+        <Navbar/>
           <Hero/>      
         </div>
       </div>
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,14 +7,18 @@ const AboutUs = () => {
   return (
     <section id='about-us' className={`${styles.flexCenter} flex-col relative`}>
       <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-[40rem]'/>
-      <h1 className =  {`${styles.heading2} text-white text-center mb-10 ss:text-[4.5rem]`}>About Us</h1>
+      <h1 className={`${styles.heading2} text-white text-center mb-10 ss:text-[4.5rem]`}>About Us</h1>
       <div className='w-full z-[1]'>
         {founders.map((founder) => (
-          <Founders key={founder.id} {...founder}/>
+          <div className="mb-8"> {/* Fixed margin */}
+            <Founders key={founder.id} {...founder}/>
+          </div>
         ))}
       </div>
     </section>
   )
 }
+
+
 
 export default AboutUs
