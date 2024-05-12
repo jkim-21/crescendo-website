2fb5427575ffc2a085d8e9f709f304f7f87a40be
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-05-11 22:23:01 -0400

    Added images to index

--- client/src/data/chapter-details.js
+++ client/src/data/chapter-details.js
@@ -316,10 +316,10 @@ export const stevenson = {
     ],
     officers: [
         {
-        id: "kriti-mohan",
-        name: "Kriti Mohan",
-        title: "Co-President",
-        headshot: kriti,
+            id: "kriti-mohan",
+            name: "Kriti Mohan",
+            title: "Co-President",
+            headshot: kriti,
         },
         {
             id: "amelia-raymond",
