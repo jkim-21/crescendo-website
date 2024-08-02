6da2eec05eea9032a4c9ea1eb57487aac5192d35
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-08-02 16:32:22 -0400

    Provide proper scroll location to hash links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,29 @@ import {styles} from './style'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './themes/theme';
 
+
+useEffect(() => {
+  const handleHashChange = () => {
+    if (window.location.hash) {
+      const id = window.location.hash.substring(1)
+      const element = document.getElementById(id);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth'});
+        }, 200)
+      }
+    }
+  };
+
+  window.addEventListener('hashchange', handleHashChange);
+  handleHashChange();
+
+  return () => {
+    window.removeEventListener('hashchange', handleHashChange);
+  };
+}, []);
+
+
 const App = () => {
   return (
       <AnimationLayout>
