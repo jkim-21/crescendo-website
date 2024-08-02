17c408839e3717eb1267abbc433f0d26533422c3
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-08-02 19:51:38 -0400

    Remove axios import

--- client/src/components/Tools/MentorMenteeMatching/MentorMenteePairing.jsx
+++ client/src/components/Tools/MentorMenteeMatching/MentorMenteePairing.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useCallback } from 'react';
-import axios from 'axios';
 import Navbar from '../../Navbar';
 import { useDropzone } from 'react-dropzone';
 import FileUpload from './FileUpload';
