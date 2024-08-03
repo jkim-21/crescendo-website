538ba96e80daaa0884dd719a0326d58ddd35d388
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-08-03 03:37:52 -0400

    Add import of firebase via cdn

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -1,15 +1,15 @@
 // firebaseConfig.jsx
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: 'crescendo-website-5cb88.firebaseapp.com',
-    projectId: 'crescendo-website-5cb88',
-    storageBucket: 'crescendo-website-5cb88.appspot.com',
-    messagingSenderId: '49917734198',
-    appId: '1:49917734198:web:48c3598fe3d909496a5797',
-    measurementId: 'G-N6YBPJDFX3'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "crescendo-website-5cb88.firebaseapp.com",
+  projectId: "crescendo-website-5cb88",
+  storageBucket: "crescendo-website-5cb88.appspot.com",
+  messagingSenderId: "49917734198",
+  appId: "1:49917734198:web:48c3598fe3d909496a5797",
+  measurementId: "G-N6YBPJDFX3",
 };
 
 const app = initializeApp(firebaseConfig);
