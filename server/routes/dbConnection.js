6114d85ee5543c68073c3b6b0e789656818299d8
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-09-13 16:16:49 -0500

    Create connection to database production

--- server/routes/dbConnection.js
+++ server/routes/dbConnection.js
@@ -3,11 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const dbUrl = process.env.JAWSDB_URL;
+
+const dbConfig = process.env.JAWSDB_URL
+  ? new URL(process.env.JAWSDB_URL)
+  : {
+      host: "127.0.0.1",
+      user: "root",
+      password: process.env.DB_PASSWORD,
+      database: "USER_INFO",
+    };
+
 const db = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: process.env.DB_PASSWORD,
-  database: "USER_INFO",
+  host: dbConfig.hostname || dbConfig.host,
+  user: dbConfig.username || dbConfig.user,
+  password: dbConfig.password || dbConfig.password,
+  database: dbConfig.pathname
+    ? dbConfig.pathname.substring(1)
+    : dbConfig.database,
   connectionLimit: 10,
 });
 
