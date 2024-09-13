2c79872ae89fbe5c206b910ab84c874bc04adaaa
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-09-13 17:09:11 -0500

    Reconfigure the production and local database connection

--- server/routes/dbConnection.js
+++ server/routes/dbConnection.js
@@ -8,19 +8,17 @@ const dbUrl = process.env.JAWSDB_URL;
 const dbConfig = process.env.JAWSDB_URL
   ? new URL(process.env.JAWSDB_URL)
   : {
-      host: "127.0.0.1",
-      user: "root",
+      hostname: "127.0.0.1",
+      username: "root",
       password: process.env.DB_PASSWORD,
-      database: "USER_INFO",
+      pathname: "USER_INFO",
     };
 
 const db = mysql.createPool({
-  host: dbConfig.hostname || dbConfig.host,
-  user: dbConfig.username || dbConfig.user,
-  password: dbConfig.password || dbConfig.password,
-  database: dbConfig.pathname
-    ? dbConfig.pathname.substring(1)
-    : dbConfig.database,
+  host: dbConfig.hostname,
+  user: dbConfig.username,
+  password: dbConfig.password,
+  database: dbUrl ? dbConfig.pathname.substring(1) : dbConfig.pathname,
   connectionLimit: 10,
 });
 
